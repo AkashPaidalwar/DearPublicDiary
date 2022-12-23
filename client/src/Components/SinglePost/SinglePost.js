f0027d58f701cbc484969e71c543e71949279d45
AkashPaidalwar <paidalwar.akash@gmail.com>
2022-12-23 19:44:17 +0530

    Update SinglePost.js

--- client/src/Components/SinglePost/SinglePost.js
+++ client/src/Components/SinglePost/SinglePost.js
@@ -7,7 +7,7 @@ export default function SinglePost() {
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
   const { user } = useContext(userContext);
-  const publicFolder = "images/";
+  const publicFolder = "images/"
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [updateMode, setUpdateMode] = useState(false);
