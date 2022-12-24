637b4332751d6c38450f4057f9e280e5833ff267
AkashPaidalwar <paidalwar.akash@gmail.com>
2022-12-24 15:08:02 +0530

    build

---
+++ client/build/precache-manifest.fff9e7f9c3bd1382a2c407235e0d6bce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb89b8b00ddf3d50326",
+    "url": "/static/css/main.6da371d9.chunk.css"
+  },
+  {
+    "revision": "4bb89b8b00ddf3d50326",
+    "url": "/static/js/main.4bb89b8b.chunk.js"
+  },
+  {
+    "revision": "f6b54bdda6c5451e2e98",
+    "url": "/static/js/1.f6b54bdd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "493fd9b0b0ab58955da2ea45e336c141",
+    "url": "/static/media/Akash.493fd9b0.png"
+  },
+  {
+    "revision": "8a3333d1bfce3ade3b81c15f783bcf7a",
+    "url": "/index.html"
+  }
+];
