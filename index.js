1da667ed224e19a37207b4d1f674825c7a486c05
AkashPaidalwar <paidalwar.akash@gmail.com>
2023-01-02 06:06:02 +0530

    q

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post("/api/upload", async (req, res) => {
 
   try {
     console.log("this line in api/upload ic called")
-    console.log(req)
+    console.log(req.body)
     let response = await fetch(
       `https://www.filestackapi.com/api/store/S3?key=AUvPiLE8TkaaaCDM2hZRQz&filename=${req.body.name}`,
       {
